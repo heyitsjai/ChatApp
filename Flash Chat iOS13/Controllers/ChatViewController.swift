e6f810af4cbc716f2656ecaf4bec7aa3a2600e97
Jai Agrawal <jagrawa2@illinois.edu>
2019-11-20 09:42:47 -0800

    message cell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -21,6 +21,7 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier )
 
     }
     
@@ -29,7 +30,7 @@ class ChatViewController: UIViewController {
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         let firebaseAuth = Auth.auth()
-        do {
+        do {    
             try firebaseAuth.signOut()
             navigationController?.popToRootViewController(animated: true)
         } catch let signOutError as NSError {
@@ -47,8 +48,8 @@ extension ChatViewController: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text =  messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text =  messages[indexPath.row].body
         return cell
     }
     
--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -1,13 +1,22 @@
-
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Jai Agrawal on 11/19/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
 
 import UIKit
 
 class MessageCell: UITableViewCell {
 
     @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5 
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
